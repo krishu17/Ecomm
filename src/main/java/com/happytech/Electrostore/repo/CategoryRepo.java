127368eab7e432cec89cb7e6077f2460c67d64dc
radhekrishna02 <morekrishna067@gmail.com>
2023-03-30 00:10:22 +0530

    Revert "user module completed"
    This reverts commit 69144e93ed11abeb7b07c0bd0b4e45df9667f99c.

--- src/main/java/com/happytech/Electrostore/repo/CategoryRepo.java
+++
@@ -1,8 +0,0 @@
-package com.happytech.Electrostore.repo;
-
-import com.happytech.Electrostore.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepo extends JpaRepository<Category,Long> {
-
-}
